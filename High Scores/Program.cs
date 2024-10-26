29eeb106ec5361c7aa0ed4e5849f8e30b74a41ab
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:34:13 +0300

    Добавьте файлы проекта.

---
+++ High Scores/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HighScores
+{
+    private readonly List<int> _scores;
+
+    // Конструктор, який приймає список балів
+    public HighScores(List<int> scores)
+    {
+        _scores = scores;
+    }
+
+    // Повертає всі бали
+    public List<int> Scores()
+    {
+        return new List<int>(_scores);
+    }
+
+    // Повертає останній доданий бал
+    public int Latest()
+    {
+        return _scores.Last();
+    }
+
+    // Повертає найвищий бал
+    public int PersonalBest()
+    {
+        return _scores.Max();
+    }
+
+    // Повертає три найвищі бали у порядку спадання
+    public List<int> PersonalTopThree()
+    {
+        return _scores.OrderByDescending(score => score).Take(3).ToList();
+    }
+}
